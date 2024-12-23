ab85a1e42ff1f7a80b4b20a440c21370816a8192
filipegeric <filip.egeric@gmail.com>
2024-12-23 16:45:38 +0100

    Add local db

--- src/main/kotlin/com/example/SpringJooqSampleApplication.kt
+++ src/main/kotlin/com/example/SpringJooqSampleApplication.kt
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import kotlin.time.measureTimedValue
 
 @SpringBootApplication
 class SpringJooqSampleApplication {
@@ -36,21 +37,26 @@ class ExampleController(private val create: DSLContext) {
     private val logger = LoggerFactory.getLogger(ExampleController::class.java)
 
     @GetMapping("/example")
-    suspend fun example(): Flow<Map<String, Any?>> {
+    suspend fun example(): List<Map<String, Any?>> {
         logger.info("Running example...")
         val fields = CITY.fields()
         val query = create
             .select(*fields)
             .from(CITY)
+            .orderBy(CITY.ID.desc())
             .limit(10)
 
-        return query.fetchAsFlow().map { record ->
-            fields.associate { it.name to record.get(it) }
+        val (result, duration) = measureTimedValue {
+            query.awaitList().map { record ->
+                fields.associate { it.name to record.get(it) }
+            }
         }
+
+        logger.info("That took: $duration")
+
+        return result
     }
 }
 
-private suspend fun <T : Record> ResultQuery<T>.awaitList(): List<T> =
-    Flux.from(this).asFlow().toList()
-
+private suspend fun <T : Record> ResultQuery<T>.awaitList(): List<T> = fetchAsFlow().toList()
 private fun <T : Record> ResultQuery<T>.fetchAsFlow(): Flow<T> = Flux.from(this).asFlow()
