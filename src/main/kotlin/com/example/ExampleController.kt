b246da759c929ae99ba85a98015cc204bab2afd7
filipegeric <filip.egeric@gmail.com>
2024-12-23 19:11:54 +0100

    Remove unused functions

--- src/main/kotlin/com/example/ExampleController.kt
+++ src/main/kotlin/com/example/ExampleController.kt
@@ -2,14 +2,9 @@ package com.example
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onCompletion
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.reactive.asFlow
-import org.jooq.Record
-import org.jooq.ResultQuery
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Flux
 import java.time.Duration
 import java.time.Instant
 import kotlin.time.toKotlinDuration
@@ -28,6 +23,3 @@ class ExampleController(private val repository: CitiesRepository) {
         }
     }
 }
-
-private suspend fun <T : Record> ResultQuery<T>.awaitList(): List<T> = fetchAsFlow().toList()
-private fun <T : Record> ResultQuery<T>.fetchAsFlow(): Flow<T> = Flux.from(this).asFlow()
