2a8862453bfc08e837b462f84ad74ac9a5ba68c3
filipegeric <filip.egeric@gmail.com>
2024-12-24 18:01:28 +0100

    Remove executor

--- src/main/kotlin/com/example/JooqConfig.kt
+++ src/main/kotlin/com/example/JooqConfig.kt
@@ -1,12 +1,10 @@
 package com.example
 
-import io.r2dbc.spi.ConnectionFactory
 import org.jooq.DSLContext
 import org.jooq.impl.DSL
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import java.util.concurrent.Executors
 
 @Configuration
 class JooqConfig {
@@ -14,7 +12,7 @@ class JooqConfig {
     @Bean
     @Profile("jdbc")
     fun jdbcDslContext(configuration: org.jooq.Configuration): DSLContext =
-        DSL.using(configuration.set(Executors.newVirtualThreadPerTaskExecutor()))
+        DSL.using(configuration)
 
 //    @Bean
 //    @Profile("r2dbc")
