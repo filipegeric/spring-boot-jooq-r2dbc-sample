363d9b54c364047387cd851d745dd0090a46836b
filipegeric <filip.egeric@gmail.com>
2024-12-27 20:31:03 +0100

    Remove jdbc

--- src/main/kotlin/com/example/jooq/JooqConfig.kt
+++ src/main/kotlin/com/example/jooq/JooqConfig.kt
@@ -1,21 +1,14 @@
 package com.example.jooq
 
+import io.r2dbc.spi.ConnectionFactory
 import org.jooq.DSLContext
 import org.jooq.impl.DSL
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
 @Configuration
 class JooqConfig {
-
     @Bean
-    @Profile("jdbc")
-    fun jdbcDslContext(configuration: org.jooq.Configuration): DSLContext =
-        DSL.using(configuration)
-
-//    @Bean
-//    @Profile("r2dbc")
-//    fun r2dbcDslContext(connectionFactory: ConnectionFactory): DSLContext =
-//        DSL.using(connectionFactory)
+    fun r2dbcDslContext(connectionFactory: ConnectionFactory): DSLContext =
+        DSL.using(connectionFactory)
 }
--- src/main/kotlin/com/example/jooq/R2dbcJooqExecutor.kt
+++ src/main/kotlin/com/example/jooq/R2dbcJooqExecutor.kt
@@ -7,12 +7,10 @@ import kotlinx.coroutines.reactive.awaitSingle
 import org.jooq.DMLQuery
 import org.jooq.Record
 import org.jooq.ResultQuery
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 
 @Component
-@Profile("r2dbc")
 class R2dbcJooqExecutor : JooqExecutor {
     override suspend fun <T : Record> select(query: ResultQuery<T>): List<T> =
         Flux.from(query).asFlow().toList()
